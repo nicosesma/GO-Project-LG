f40a25c4e68ee9b32c2499d08ad0291a660d8f1f
nicosesma <nicosm310@gmail.com>
2016-08-03 11:10:52 -0700

    Created game logic in front end and styled the table to make it mor UX friendly

--- src/board.js
+++ src/board.js
@@ -1,47 +1,47 @@
-export class Board {
-  constructor() {
-    this.board = [
-      null, null, null,
-      null, null, null,
-      null, null, null
-    ]
-    this.xsTurn = true
-  }
-
-  nextToMove() {
-    return this.xsTurn ? 'X' : 'O'
-  }
-
-  isEmpty() {
-    console.log('inside this.isEmpty')
-    console.log(this.board)
-    return this.board.filter(function(cell){ return cell === null }).length === 9;
-  }
-
-  display() {
-    console.log(this.board)
-  }
-
-  place(letter, x, y) {
-    this.board[y][x] = letter
-  }
-
-  makeBestMove() {
-    // calculate potential opponent winning columns/diagonals
-      // if any of them already have two opponent pieces, place on the third
-      // if any of them have one opponent piece (and none of mine), randomly pick one
-      // if no potential wins for opponent, randomly pick one
-  }
-
-  rowScores() {
-
-  }
-
-  rowToScore(row, mine) {
-
-  }
-
-}
+// export class Board {
+//   constructor() {
+//     this.board = [
+//       null, null, null,
+//       null, null, null,
+//       null, null, null
+//     ]
+//     this.xsTurn = true
+//   }
+//
+//   nextToMove() {
+//     return this.xsTurn ? 'X' : 'O'
+//   }
+//
+//   isEmpty() {
+//     console.log('inside this.isEmpty')
+//     console.log(this.board)
+//     return this.board.filter(function(cell){ return cell === null }).length === 9;
+//   }
+//
+//   display() {
+//     console.log(this.board)
+//   }
+//
+//   place(letter, x, y) {
+//     this.board[y][x] = letter
+//   }
+//
+//   makeBestMove() {
+//     // calculate potential opponent winning columns/diagonals
+//       // if any of them already have two opponent pieces, place on the third
+//       // if any of them have one opponent piece (and none of mine), randomly pick one
+//       // if no potential wins for opponent, randomly pick one
+//   }
+//
+//   rowScores() {
+//
+//   }
+//
+//   rowToScore(row, mine) {
+//
+//   }
+//
+// }
 
 // let b = new Board()
 // b.display()
--- src/public/front-end.js
+++ src/public/front-end.js
@@ -1,14 +1,117 @@
 "use strict";
 
-$(document).ready(function(){
-  $('.square').click(function() {
-    let value = $(this).html()
-    $(this).html(value === 'X' ? 'O' : 'X')
-  })
-})
-
 let createTable = ()=>{
   $.get('/games/new', function (result) {
     console.log('Got my ajax request', result)
   })
 }
+
+// $(document).ready(function(){
+//   $('.square').click(function() {
+//     let value = $(this).html()
+//     $(this).html(value === 'X' ? 'O' : 'X')
+//   })
+// })
+//
+
+$(document).ready(function(){
+    var turnCount=0;
+
+
+      $('.board').find('td').click(function(){
+        if ($(this).html() === "") {
+            if (turnCount % 2 === 0){
+              $(this).html('X');
+              checkVictory('X');
+            } else {
+           //player 2's turn (O)
+              $(this).html('O');
+              checkVictory('O');
+            }
+          turnCount++;
+        }
+      });
+
+
+    function checkVictory(player){
+       //top row check
+        if ($('.board').find('#1').html() !== ''){
+            if ($('.board').find('#1').html() == $('.board').find('#2').html()) {
+                if ($('.board').find('#1').html() == $('#3').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        //left column check
+            if ($('.board').find('#1').html() == $('.board').find('#4').html()) {
+                if ($('.board').find('#1').html() == $('#7').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        //left diagonal check
+            if ($('.board').find('#1').html() == $('.board').find('#5').html()) {
+                if ($('.board').find('#1').html() == $('#9').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        }
+
+       //middle column check
+        if ($('.board').find('#2').html() !== ''){
+            if ($('.board').find('#2').html() == $('.board').find('#5').html()) {
+                if ($('.board').find('#2').html() == $('#8').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        }
+
+        //right column check
+        if ($('.board').find('#3').html() !== ''){
+            if ($('.board').find('#3').html() == $('.board').find('#6').html()) {
+                if ($('.board').find('#3').html() == $('#9').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+            //right diag check
+            if ($('.board').find('#3').html() == $('.board').find('#5').html()) {
+                if ($('.board').find('#3').html() == $('#7').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        }
+
+        //middle row check
+
+        if ($('.board').find('#4').html() !== ''){
+            if ($('.board').find('#4').html() == $('.board').find('#5').html()) {
+                if ($('.board').find('#4').html() == $('#6').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        }
+
+        //bottom row check
+        if ($('.board').find('#7').html() !== ''){
+            if ($('.board').find('#7').html() == $('.board').find('#8').html()) {
+                if ($('.board').find('#7').html() == $('#9').html()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        } //else if ($('.board').find('#7').html() !== ''){
+        //     if ($('.board').find('#7').html() !== $('.board').find('#8').html()) {
+        //       if ($('.board').find('#7').html() !== $('board').find('#9').html())
+        //       alert('Game over!  YOU BOTH LOSE!!');
+        //       $('#newGame').removeClass('invisible');
+      }
+    $('#newGame').click(function(){
+        $('.square').html('');
+    });
+
+});
